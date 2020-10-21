cb94795abffcd91fbe7735736f80b0ecbeb48285
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-10-21 10:23:09 -0700

    Edited App.css and StoryBashBoard.js

--- expat/src/components/StoryDashBoard.js
+++ expat/src/components/StoryDashBoard.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import {axiosWithAuth} from '../utils/axiosWithAuth';
 
-import StoryForm from './StoryForm';
-import StoryList from './StoryList';
-
-
-
 export default function StoryDashBoard(props) {
     const [posts, setPosts] = useState([]);
     const [update, setUpdate] = useState(false)
@@ -14,17 +9,47 @@ export default function StoryDashBoard(props) {
         axiosWithAuth()
         .get('/api/posts')
         .then(res => {
+            console.log(res.data)
             setPosts(res.data)
         })
         .catch(err => console.log("error DashBoard", err))
-    }, [update])
+    }, [])
 
+    const deleteHandler = () => {
+        axiosWithAuth()
+          .delete(``)
+          .then((res) => {
+            debugger
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
 
-    return(
-        <div className='dash'>
-            <StoryForm posts={posts} updatePosts={setPosts}
-            setUpdating={setUpdate} updating={update}/>
-            <StoryList posts={posts}/>
+    const editHandler = (postId) => {
+        console.log(postId)
+    }
+
+    return (
+        <div className='parent'>
+            {posts.map(singlePost => (
+                <div className='container' key={singlePost.postId}>
+                
+                <div className='thumbnail'>
+                    <img src={singlePost.imageURL} alt='' />
+                </div>
+
+                <div className='preview'> 
+                    <h3>{singlePost.title}</h3>
+                    <h5>{singlePost.description}</h5>
+                </div>
+                
+                <button onClick={() => deleteHandler()}>Delete</button>
+                <button onClick={() => editHandler(singlePost.postId)}>Edit</button>
+                </div>
+            )
+            )}
+
         </div>
     )
 }
