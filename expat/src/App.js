7775d0e4f7c9611197a0c25b8cf7b7960d84571f
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-22 19:35:48 -0400

    finished

--- expat/src/App.js
+++ expat/src/App.js
@@ -3,7 +3,7 @@ import Footer from './components/Footer';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import './styles/App.css';
-import PrivateRoute from './utils/PrivateRoute';
+
 import StoryDashBoard from './components/StoryDashBoard';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from './components/Header'
@@ -11,8 +11,7 @@ import EditStory from './components/EditStory';
 import AddStory from './components/AddStory';
 import AddComment from './components/AddComment';
 import HomePage from './components/HomePage';
-// import Main from './components/Main';
-// import max3 from './images/max3.png';
+
 
 
 
@@ -21,12 +20,11 @@ function App() {
 
   return (<Router>
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
       <Header />
-      {/* <Switch>
-        <PrivateRoute path="/posts" component={StoryDashBoard}/>
-        <Route exact path="/Signin" component={Signin} />
-      </Switch> */}
+      </header>
+     
+     
      
       
      <Route 
@@ -64,11 +62,15 @@ function App() {
       
 
 
-      {/* <Route path="/Footer"> */}
-       <Footer className="maxFoot" />
-        {/* </Route> */}
+     </div>
+    <div>
+      <Footer className="maxFoot" />
+      
+    </div>   
+  
+       
 
-    </div>
+   
 
 
   </Router>
